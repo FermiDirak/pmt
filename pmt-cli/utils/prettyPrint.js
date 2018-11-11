223ccb73896b295ff8cfb1e0c6b3a1ab654b035b
Bryan Manuele <bryan@flexport.com>
2018-11-11 15:02:51 -0800

    organized project directory

---
+++ pmt-cli/utils/prettyPrint.js
@@ -0,0 +1,32 @@
+const chalk = require('chalk');
+
+/** logs a command in a pretty way
+ * @param command The command being ran
+ * @param param The param associated with the command
+ * @param options The options being passed in with the command
+ */
+const printCommand = (command, param, ...options) => {
+  let formattedOptions = '';
+
+  options.forEach(option => {
+    if (option[1]) {
+      formattedOptions += `${option[0]} ${chalk.yellow(option[1])}`;
+    }
+  });
+
+  console.log(
+    `Just ran: pmt ${command} ${chalk.magenta(param)} ${formattedOptions}`
+  );
+}
+
+/** prints out an error message in a formatted way
+ * @param errorMessage The error mesage to prettify
+ */
+const printError = (errorMessage) => {
+  console.log(Chalk.red('Error: ') + errorMessage);
+}
+
+module.exports = {
+  printCommand,
+  printError,
+};
