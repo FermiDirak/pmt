8d3e057af7b1b85dd8b754fd668ac7db3a2ff1ec
Bryan Manuele <bryan@flexport.com>
2018-11-17 13:59:00 -0800

    linting

--- pmt-cli/datastructures/story.js
+++ pmt-cli/datastructures/story.js
@@ -19,18 +19,14 @@ class Story {
    * @param task {Task} The task to look for
    * @return {boolean} whether the task is included or not */
   includesTask(task) {
-    return this.tasks.some(memberTask => {
-      return memberTask.commitHash === task.commitHash;
-    }
+    return this.tasks.some(memberTask => memberTask.commitHash === task.commitHash);
   }
 }
 
 /** deserializes a story in object form into a Story instance
  * @param storyObject {object} The story object to be transformed
  * @return {Story} the story instance */
-Story.deserialize = (storyObject) => {
-  return new Story(storyObject.id, storyObject.descriptor);
-}
+Story.deserialize = storyObject => new Story(storyObject.id, storyObject.descriptor);
 
 
 module.exports = Story;
