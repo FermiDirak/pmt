37b1ecf60338386c512b83d6c3ebd133c8776746
Bryan Manuele <bryan@flexport.com>
2019-05-25 21:56:53 -0700

    cleanup

--- pmt-cli/target/rls/debug/deps/pmt_cli-15c48806d00ce351.d
+++
@@ -1,5 +0,0 @@
-/Users/bryanmanuele/Projects/pmt/pmt-cli/target/rls/debug/deps/pmt_cli-15c48806d00ce351.rmeta: src/main.rs
-
-/Users/bryanmanuele/Projects/pmt/pmt-cli/target/rls/debug/deps/pmt_cli-15c48806d00ce351.d: src/main.rs
-
-src/main.rs:
