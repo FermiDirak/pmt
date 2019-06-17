ce5fe45361bcc9ee672ed284bf524bcc879f3b51
Bryan Manuele <bryan@flexport.com>
2019-06-16 20:33:14 -0700

    spawns detached process!

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 extern crate clap;
 
 use std::process;
-use clap::{App, SubCommand, crate_version, crate_description, crate_authors};
+use std::env;
+use std::process::Command;
 
-// struct Cli {
-//     pattern: String,
-//     path: std::path::PathBuf,
-// }
+use clap::{
+    App,
+    SubCommand,
+    AppSettings,
+    crate_version,
+    crate_description,
+    crate_authors
+};
 
 fn main() {
     let matches = App::new("pmt")
         .version(crate_version!())
         .about(crate_description!())
         .author(crate_authors!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::AllowExternalSubcommands)
         .subcommand(SubCommand::with_name("note")
             .about("takes a note localized within the current git repo"))
         .subcommand(SubCommand::with_name("notes")
@@ -29,17 +36,17 @@ fn main() {
         process::exit(0);
     }
 
-    println!("test: {:?}", matches);
+    if let Some(_subcommand) = matches.subcommand_name() {
+        let args: Vec<String> = env::args().collect();
+        let args = &args[1..];
 
-    println!("test")
+        println!("running: `{:?}`", args);
 
-    // let pattern = std::env::args().nth(1).expect("no pattern given");
-    // let path = std::env::args().nth(2).expect("no path given");
+        let _output = Command::new("git")
+            .args(args)
+            .spawn()
+            .expect("failed to execute process");
 
-    // let args = Cli {
-    //     pattern: pattern,
-    //     path: std::path::PathBuf::from(path),
-    // };
-
-    // println!("Hello, world! {} {:#?}", args.pattern, args.path);
+        process::exit(0);
+    }
 }
